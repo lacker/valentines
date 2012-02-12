f601f604c2fc7b725b97023ec96a97e0be2c6953
Kevin Lacker <lacker@gmail.com>
2012-02-11 17:18:00 -0800

    fiddle with sensitivity

--- 2012/wordsquare.js
+++ 2012/wordsquare.js
@@ -187,9 +187,38 @@ var Position = function(event) {
   var canvas = $("canvas")[0];
   this.pixelX = event.pageX - canvas.offsetLeft;
   this.pixelY = event.pageY - canvas.offsetTop;
+
+  // The location in grid units instead of pixels
+  this.floatX = (this.pixelX - PADDING) / CELL_SIZE;
+  this.floatY = (this.pixelY - PADDING) / CELL_SIZE;
+
+  // The actual grid unit this maps to
+  this.x = Math.floor(this.floatX);
+  this.y = Math.floor(this.floatY);
+
+  // Figure out whether this should trigger something.
+  // We only trigger for cell clicks
+  if (this.x < 0 || this.x >= LEN ||
+      this.y < 0 || this.y >= LEN) {
+    return;
+  }
+
+  // We don't trigger if you're within the buffer from the edge.
+  var buffer = 0.3;
+  var xDiff = this.floatX - this.x;
+  var yDiff = this.floatY - this.y;
+  if (xDiff < buffer || xDiff > 1 - buffer ||
+      yDiff < buffer || yDiff > 1 - buffer) {
+    return;
+  }
+
+  this.trigger = true;
+  return;
 };
 
 Position.prototype = {
+  trigger: false,
+  
   toString: function() {
     return "" + this.pixelX + " : " + this.pixelY;
   }
@@ -197,17 +226,23 @@ Position.prototype = {
 
 function down(e) {
   var pos = new Position(e);
-  console.log("down: " + pos);
+  if (pos.trigger) {
+    console.log("down: " + pos);
+  }
 }
 
 function move(e) {
   var pos = new Position(e);
-  console.log("move: " + pos);
+  if (pos.trigger) {
+    console.log("move: " + pos);
+  }
 }
 
 function up(e) {
   var pos = new Position(e);
-  console.log("up: " + pos);
+  if (pos.trigger) {
+    console.log("up: " + pos);
+  }
 }
 
 
