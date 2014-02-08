b007e65f3eeb4136851ef9b2993ee7ac388134b9
Kevin Lacker <lacker@gmail.com>
2014-02-08 14:11:38 -0800

    Scaling for testing and preventing UI element selection

--- 2014/FlappyAlex/public/flappy.js
+++ 2014/FlappyAlex/public/flappy.js
@@ -1,7 +1,11 @@
 // Designed for an iPad running at 2048 x 1536 resolution held vertically.
-
 var GLOBALS = {};
 var container = null;
+
+// Scales things so we can test in a browser
+function scale(scalar) {
+  return Math.round(scalar * 0.2);
+}
 
 function show_intro() {
   var div = $("<div>");
@@ -15,12 +19,14 @@ function show_intro() {
 
 function start_game() {
   var alex = $("<div>");
-  alex.width(235);
-  alex.height(277);
+  alex.width(scale(235));
+  alex.height(scale(277));
   alex.css("position", "absolute");
   alex.css("left", "0px");
   alex.css("top", "0px");
   var img = $('<img id="dynamic">');
+  img.width(scale(235));
+  img.height(scale(277));
   img.attr('src', "./alex.png");
   alex.append(img);
 
@@ -30,13 +36,21 @@ function start_game() {
   }
 }
 
+function stop_game() {
+}
+
 $(function() {
   var container = $("#container");
-  container.height(2048);
-  container.width(1536);
+  container.height(scale(2048));
+  container.width(scale(1536));
   container.css("position", "relative");
   container.css("margin-top", "0px");
   container.css("background-color", "#eee");
   container.on("click tap", function() { GLOBALS.tap(); });
+  window.setInterval(function() {
+    if (GLOBALS.tick) {
+      GLOBALS.tick();
+    }
+  }, 50);
   show_intro();
 });
