4f1a1fbcf750bd1ddc33536c360845ba7a4b6511
Kevin Lacker <lacker@gmail.com>
2015-02-07 16:43:57 -0800

    Reactify the footer

--- 2015/TheGreatEscape/public/escape.jsx
+++ 2015/TheGreatEscape/public/escape.jsx
@@ -8,11 +8,26 @@ var GameBoard = React.createClass({
   }
 })
 
+var Message = React.createClass({
+  render: function() {
+    return (
+      <div className="message">
+      Alex is trying to escape! Trap him by creating toy barriers.
+      </div>
+    );
+  }
+})
+
 $(document).ready(function() {
 
   React.render(
     <GameBoard />,
     document.getElementById("box")
+  );
+
+  React.render(
+    <Message />,
+    document.getElementById("footer")
   );
 
   console.log("called ready")
