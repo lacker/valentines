948e34d84ab2346327e6198fa58d83d7f6f16099
Kevin Lacker <lacker@gmail.com>
2015-02-07 16:17:10 -0800

    react rendering actually happening

--- 2015/TheGreatEscape/public/escape.jsx
+++ 2015/TheGreatEscape/public/escape.jsx
@@ -1,3 +1,22 @@
-function test() {
-  console.log("HELLO WORLD")
-}
+var GameBoard = React.createClass({
+  render: function() {
+    return (
+      <div className="gameboard">
+      Hello, world! I am the GameBoard.
+      </div>
+    );
+  }
+})
+
+$(document).ready(function() {
+
+  React.render(
+    <GameBoard />,
+    document.getElementById("box")
+  );
+
+  console.log("called ready")
+})
+
+console.log("escape.jsx loaded")
+
