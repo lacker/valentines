a6908524b2b2869344485e75dd31298c27ecf6c9
Kevin Lacker <lacker@gmail.com>
2015-02-08 14:08:09 -0800

    got multiple cells displaying

--- 2015/TheGreatEscape/public/escape.jsx
+++ 2015/TheGreatEscape/public/escape.jsx
@@ -1,8 +1,29 @@
+var Cell = React.createClass({
+  render: function() {
+    var top = 50 + 50 * this.props.row 
+    var left = 50 * this.props.col
+    var style = {
+      top: "" + top + "px",
+      left: "" + left + "px"
+    }
+    return (
+      <div className="cell" style={style}>
+      {this.props.content}
+      </div>
+    );
+  }
+})
+
 var GameBoard = React.createClass({
   render: function() {
     return (
       <div className="gameboard">
       Hello, world! I am the GameBoard.
+      <Cell content="cell 0,0" row={0} col={0}/>
+      <Cell content="cell 0,1" row={0} col={1}/>
+      <Cell content="cell 1,1" row={1} col={1}/>
+      <Cell content="cell 1,0" row={1} col={0}/>
+      {this.props.cells}
       </div>
     );
   }
