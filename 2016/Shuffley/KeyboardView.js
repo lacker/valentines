64fc563caa7abe30d4ce798fccea191c5ca936c4
Kevin Lacker <lacker@gmail.com>
2016-03-26 14:34:45 -0700

    arrange the keyboard view to look better

--- 2016/Shuffley/KeyboardView.js
+++ 2016/Shuffley/KeyboardView.js
@@ -1,7 +1,9 @@
 'use strict';
 import React, {
   Component,
-  TextInput
+  Text,
+  TextInput,
+  View
 } from 'react-native';
 
 export default class KeyboardView extends Component {
@@ -13,14 +15,38 @@ export default class KeyboardView extends Component {
 
   render() {
     // TODO: all caps
-    // TODO: show the keyboard
     // TODO: show a message
     // TODO: add the word on "enter"
 
     return (
-      <TextInput
-        style={{height: 40, borderColor: 'gray', borderWidth: 1}} onChangeText={(text) => this.setState({text})}
-        value={this.state.text} />
+      <View style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'space-around'
+        }}>
+        <View style={{flex: 1, justifyContent: 'center'}}>
+          <TextInput
+            style={{
+              height: 40,
+              borderColor: 'gray',
+              borderWidth: 1,
+              width: 200
+            }}
+            autoFocus
+            onChangeText={(text) => this.setState({text})}
+            value={this.state.text} />
+        </View>
+        <View style={{
+            flex: 1,
+            justifyContent: 'center',
+          }}>
+          <Text
+            style={{textAlign: 'center'}}>
+            Enter a word that would be fun to unscramble!
+          </Text>
+        </View>
+        <View style={{flex: 2}} />
+      </View>
     );
   }
 }
